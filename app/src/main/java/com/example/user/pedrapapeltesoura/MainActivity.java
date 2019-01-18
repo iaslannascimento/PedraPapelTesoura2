4e8ec5458381c62bbfefcaf2a1243bdf74c41ec6
iaslannascimento <iaslan.nascimento@hotmail.com>
2019-01-18 13:55:49 -0300

    Progress

--- app/src/main/java/com/example/user/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/user/pedrapapeltesoura/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.pedrapapeltesoura;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPapel(View view){
+        this.escolha(1);
+    }
+
+    public void selecionarPedra(View view){
+        this.escolha(2);
+    }
+
+    public void selecionarTesoura(View view){
+        this.escolha(3);
+    }
+
+    public void escolha(View view){
+
     }
 }
